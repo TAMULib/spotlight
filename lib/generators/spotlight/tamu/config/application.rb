e98a1bb9f07adea5af0ae4b901f099ca1586ea03
William Welling <wwelling@library.tamu.edu>
2017-11-28 13:05:29 -0600

    remove lib join to path

--- lib/generators/spotlight/tamu/config/application.rb
+++ lib/generators/spotlight/tamu/config/application.rb
@@ -13,8 +13,6 @@ module Spotlight
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths << Rails.root.join('lib')
-
     config.to_prepare do
       Spotlight::Exhibit.send(:include, ExhibitExtension)
     end
