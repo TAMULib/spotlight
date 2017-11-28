86055f704e2c0b60634cd051d3ce1724c571d1c6
William Welling <wwelling@library.tamu.edu>
2017-11-28 12:43:21 -0600

    some more cruft removal, mirador height, navigator language

--- lib/generators/spotlight/tamu/app/models/concerns/manifest_concern.rb
+++ lib/generators/spotlight/tamu/app/models/concerns/manifest_concern.rb
@@ -9,11 +9,4 @@ module ManifestConcern
     return if manifest_url.blank? || !manifest_available?
     manifest_url
   end
-
-  def iiif_drag_n_drop(manifest, width: '40px')
-    link_url = format Settings.iiif_dnd_base_url, query: { manifest: manifest }.to_query
-    link_to link_url, class: 'iiif-dnd pull-right', data: { turbolinks: false } do
-      image_tag 'iiif-drag-n-drop.svg', width: width, alt: 'IIIF Drag-n-drop'
-    end
-  end
 end
