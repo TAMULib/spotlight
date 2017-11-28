1d23993e1cf9864866170886f177b9c4347b1c83
William Welling <wwelling@library.tamu.edu>
2017-11-28 10:59:18 -0600

    removed even more cruft

--- lib/generators/spotlight/tamu/config/traject.rb
+++
@@ -1,5 +0,0 @@
-# Stolen from https://github.com/sul-dlss/dlme/blob/master/config/traject.rb
-unless defined? Rails
-  puts 'Loading environment...'
-  require File.expand_path('../../config/environment', __FILE__)
-end
