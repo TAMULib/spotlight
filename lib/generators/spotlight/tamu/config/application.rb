aa6bb95218da6101637bd3565d16a3b50ffe646e
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2018-11-02 13:36:17 -0500

    Moved config value to cookbook template

--- lib/generators/spotlight/tamu/config/application.rb
+++ lib/generators/spotlight/tamu/config/application.rb
@@ -12,8 +12,6 @@ module Spotlight
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.mirador_url = 'https://labs.library.tamu.edu/mirador?%{query}'
-
     config.to_prepare do
       Spotlight::Exhibit.send(:include, ExhibitExtension)
     end
